79ac76859254519858e53c5298e2f5be247e18bd
JingCheng729 <80359063+JingCheng729@users.noreply.github.com>
2025-07-02 18:30:39 -0400

    Jump

--- src/unitree_ros_to_real/unitree_legged_real/include/GaitLibrary.h
+++ src/unitree_ros_to_real/unitree_legged_real/include/GaitLibrary.h
@@ -1,9 +1,9 @@
 #ifndef GAITLIBRARY_H
 #define GAITLIBRARY_H
 
-void gait_library(float (&bez_params_st)[21][72], float (&bez_params_fly)[21][72], float (&duration_times_st)[21], float (&duration_times_fly)[21]);
+void gait_library(float (&bez_params_st)[21][72], float (&bez_params_fly)[21][72], float (&bez_params_stouch)[21][72], float (&duration_times_st)[21], float (&duration_times_fly)[21], float (&duration_times_stouch)[21]);
 
-void gaitpara_interpolation(Eigen::Matrix<float, 12, 6>& coeff_st, Eigen::Matrix<float, 12, 6>& coeff_fly, float& step_time_st, float& step_time_fly, float (&bez_params_st)[21][72], float (&bez_params_fly)[21][72], float (&duration_times_st)[21], float (&duration_times_fly)[21], std::vector<float> &x_velocity, float fly_sel_velocity, float (&desired_velocity_trunk)[3], long motiontime);
+void gaitpara_interpolation(Eigen::Matrix<float, 12, 6>& coeff_st, Eigen::Matrix<float, 12, 6>& coeff_fly, Eigen::Matrix<float, 12, 6>& coeff_stouch, float& step_time_st, float& step_time_fly,float& step_time_stouch, float (&bez_params_st)[21][72], float (&bez_params_fly)[21][72], float (&bez_params_stouch)[21][72], float (&duration_times_st)[21], float (&duration_times_fly)[21], float (&duration_times_stouch)[21], std::vector<float> &x_velocity, float fly_sel_velocity, float (&desired_velocity_trunk)[3], long motiontime);
 
 void gait_control_gains(float (&joint_Kp)[12], float (&joint_Kv)[12], float (&joint_Kp_f)[12], float (&joint_Kv_f)[12], float (&joint_Kp_fd)[12], float (&joint_Kv_fd)[12], int (&flag_st)[4], int (&flag_fly)[4], float (&t_norminal_st)[4], float (&t_norminal_fly)[4], float PD_time, float transition_rate_temp_st, long motiontime);
 
--- src/unitree_ros_to_real/unitree_legged_real/src/utils/TorqueLibrary.cpp
+++ src/unitree_ros_to_real/unitree_legged_real/src/utils/TorqueLibrary.cpp
@@ -12,7 +12,7 @@
 
 #include "TorqueLibrary.h"
 
-void torque_record(float (&torque_stance)[300][12], float (&Error_stance)[300][12], float (&dError_stance)[300][12], float (&Pre_Error_stance)[300][12], float (&Pre_dError_stance)[300][12], float (&Feedforward_Error_tor_stance)[300][12], float (&pre_torque_stance_fil_zero)[300][12], float (&pre_torque_stance_fil_err_tor)[300][12], float (&total_stance_torque_fil)[300][12], float (&pre_torque_stance_fil_total)[300][12], int stride_counter)
+void torque_record(float* Torque_Profile_st, float (*torque)[12], float (*Error)[12], float (*dError)[12], float (*Pre_Error)[12], float (*Pre_dError)[12], float (*Feedforward_Error_tor)[12], float (*pre_torque_fil_zero)[12], float (*pre_torque_fil_err_tor)[12], float (*total_torque_fil)[12], float (*pre_torque_fil_total)[12], int rows, int stride_counter)
 {
     float ILC_Switch[2];
 
@@ -23,227 +23,96 @@ void torque_record(float (&torque_stance)[300][12], float (&Error_stance)[300][1
 
     kb::math::Filt<double> filt(fc);
     kb::math::FiltFilt<double> filtfilt(fc);
-    if (stride_counter > 1){
-        ////in the first 5 strides, the system will direct record the raw torque data(related to the first order filter)
-        if (stride_counter > 4 && stride_counter < 10){
-             ILC_Switch[0] = 0;
-             ILC_Switch[1] = 1/0.1;          // this value associate with the gains in first order filter belew.
+
+    ILC_Switch[0] = 1;
+    ILC_Switch[1] = 1;
+
+    for (int i_st = 0 ; i_st < rows ; i_st++){
+        for (int j_st = 0 ; j_st < 12 ; j_st++){
+            Pre_Error[i_st][j_st] = Error[i_st][j_st];
+            Pre_dError[i_st][j_st] = dError[i_st][j_st];
         }
-        else{
-             ILC_Switch[0] = 1;
-             ILC_Switch[1] = 1;
+    }
+
+    for (int j_st = 0 ; j_st < 12 ; j_st++){
+        std::vector<double> temp_vec;
+        std::vector<double> temp_vec_error;
+        temp_vec.reserve(rows);
+        temp_vec_error.reserve(rows);
+
+        for (int i_st = 0; i_st < rows; i_st++){                                     
+            temp_vec.push_back((Torque_Profile_st)[j_st*1*rows+0*rows+i_st]);
+            temp_vec_error.push_back(Feedforward_Error_tor[i_st][j_st]);
+        }
+
+        auto zeroPhaseFiltered = filtfilt.ZeroPhaseFiltering(temp_vec); 
+        auto zeroPhaseFiltered_err_tor = filtfilt.ZeroPhaseFiltering(temp_vec_error);
+
+        for (int i_st = 0; i_st < rows; i_st++){
+            pre_torque_fil_zero[i_st][j_st] = zeroPhaseFiltered[i_st];
+            pre_torque_fil_err_tor[i_st][j_st] = zeroPhaseFiltered_err_tor[i_st];
+
+            total_torque_fil[i_st][j_st] = 0.0 * ILC_Switch[0] * pre_torque_fil_total[i_st][j_st] +  1.0 * ILC_Switch[1] * pre_torque_fil_zero[i_st][j_st];
+            pre_torque_fil_total[i_st][j_st] = total_torque_fil[i_st][j_st];
+        }
+
+        for (int i_st = 0 ; i_st < rows ; i_st++){
+            for (int j_st = 0 ; j_st < 12 ; j_st++){
+                Error[i_st][j_st] = 0.0;
+                dError[i_st][j_st] = 0.0;
+            }                 
+        }
+    }
+}
+
+
+void torque_library(float* Torque_Profile_st, int size)
+{
+    // Define a stream for each file to be opened
+    std::ifstream infile_Torque_st[12][21];
+
+    // Extracting parameters for different speeds
+    for (int i_lib = 0; i_lib < 1; i_lib++) {
+        std::stringstream buff;
+        buff.precision(1);
+        buff.setf(std::ios::fixed); 
+        float order_038 = (i_lib * 10.0 - 10.0) / 10.0;   // Convert issue    
+        buff << order_038;
+        
+        std::string str, str2;
+        str = buff.str();
+
+        if (size == 439) {
+            str = "st";
+        }
+        else {
+            str = "stouch";
         }
         
+        // For torque
+        std::stringstream filename_torque[12];
 
-        for (int i_st = 0 ; i_st < 300 ; i_st++){
-            for ( int j_st = 0 ; j_st<12; j_st++){
-                Pre_Error_stance[i_st][j_st] = Error_stance[i_st][j_st];
-                Pre_dError_stance[i_st][j_st] = dError_stance[i_st][j_st];
-                //  cout  << Pre_torque_stance[0][0] << ","<< " torque_stance" << torque_stance[0][0]<< ",";           
-                }
-              }
+        for (int j_lib = 0; j_lib < 12; j_lib++) {
+            std::stringstream buff2;
+            int order = j_lib + 1;                
+            // Convert int to str
+            buff2 << order;
+            std::string str2;
+            str2 = buff2.str();
 
-        for ( int j_st = 0 ; j_st<12; j_st++){
-            std::vector<double> temp_vec;
-            std::vector<double> temp_vec_error;
-            temp_vec.reserve(300);
-            temp_vec_error.reserve(300);
+            filename_torque[j_lib] << "/home/dlar/Gait_Library/035d_04h_04t/5Jump/Jump_" << str <<  str2 <<".txt";
+            infile_Torque_st[j_lib][i_lib].open(filename_torque[j_lib].str()); // Open the file
 
-            ///per_st_motor_torque_fil_unshift[j_st] = Pre_torque_stance[0][j_st]; 
-
-            for ( int i_st = 0; i_st < 300; i_st++ ){                                     
-                temp_vec.push_back(torque_stance[i_st][j_st]);
-                temp_vec_error.push_back(Feedforward_Error_tor_stance[i_st][j_st]);
+            for (int i_sample = 0; i_sample < size; i_sample++) {
+                infile_Torque_st[j_lib][i_lib] >> Torque_Profile_st[(j_lib * 1 + i_lib) * size + i_sample]; // Read a value
+                
             }
+            infile_Torque_st[j_lib][i_lib].close(); // Close the file after reading
 
-            auto zeroPhaseFiltered  = filtfilt.ZeroPhaseFiltering(temp_vec); 
-            auto zeroPhaseFiltered_err_tor  = filtfilt.ZeroPhaseFiltering(temp_vec_error);
-
-            for ( int i_st = 0; i_st < 300; i_st++ ){
-                pre_torque_stance_fil_zero[i_st][j_st] = zeroPhaseFiltered[i_st];
-                pre_torque_stance_fil_err_tor[i_st][j_st] = zeroPhaseFiltered_err_tor[i_st];
-
-                total_stance_torque_fil[i_st][j_st] = 0.9 * ILC_Switch[0] * pre_torque_stance_fil_total[i_st][j_st] +  0.1 * ILC_Switch[1] * pre_torque_stance_fil_zero[i_st][j_st];
-                pre_torque_stance_fil_total[i_st][j_st] = total_stance_torque_fil[i_st][j_st];
-            }
-
-            for (int i_st = 0 ; i_st < 300 ; i_st++){
-                for ( int j_st = 0 ; j_st<12; j_st++){
-                    Error_stance[i_st][j_st] = 0.0;
-                    dError_stance[i_st][j_st] = 0.0;
-                }                 
-            }             
-
-        }              
+        }
     }
-
 }
 
-void torque_library(float (&Torque_Profile_st)[12][21][230])
-{
-    /////////**********stand_phase////////////
-	        //load array //define a place to store
-
-    // float Torque_Profile_st_FRThigh[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_FRCalf[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_FLThigh[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_FLCalf[21][230] = { 0.0 };//define a place to store toruqe
-
-    // float Torque_Profile_st_RRThigh[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_RRCalf[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_RLThigh[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st_RLCalf[21][230] = { 0.0 };//define a place to store toruqe
-    // float Torque_Profile_st[12][21][230] = { 0.0 };//define a place to store toruqe
-
-
-
-    // std::ifstream infile_Torque_st_FRThigh[21];//define a stream
-    // std::ifstream infile_Torque_st_FRCalf[21];//define a stream
-    // std::ifstream infile_Torque_st_FLThigh[21];//define a stream
-    // std::ifstream infile_Torque_st_FLCalf[21];//define a stream
-
-    // std::ifstream infile_Torque_st_RRThigh[21];//define a stream
-    // std::ifstream infile_Torque_st_RRCalf[21];//define a stream
-    // std::ifstream infile_Torque_st_RLThigh[21];//define a stream
-    // std::ifstream infile_Torque_st_RLCalf[21];//define a stream
-    std::ifstream infile_Torque_st[12][21];//define a stream
-
-    //Extracting paremeters of different speeds
-    for(int i_lib=0; i_lib<21; i_lib++){
-        std::stringstream buff;
-            buff.precision(1);
-            buff.setf(std::ios::fixed); 
-            float order_038 = (i_lib * 1.0 - 10.0)/10.0;   //convert issue    
-            buff<<order_038;
-           
-            std::string str;
-            str = buff.str();
-            /////////////////////////for torque/////////////////
-            std::stringstream filename_torque[12];
-
-            for (int j_lib=0; j_lib<12; j_lib++){
-                std::stringstream buff2;
-                int order = j_lib + 1;                
-                ///////// convert int to str ///////
-                buff2<<order;
-                std::string str2;
-                str2 = buff2.str();
-
-                filename_torque[j_lib] << "/home/dlar/Gait_Library/Torquelib0723/" << str2 << "th_Torque_v"<< str << ".txt";
-                infile_Torque_st[j_lib][i_lib].open(filename_torque[j_lib].str());//open the file
-
-               //debugging tool//j_lib=11//////////////
-                // if (i_lib == 14 && j_lib == 11)
-                // {
-                //   std::ofstream file1("Torque_Profile_st1204.txt");
-                //     file1 << filename_torque[j_lib].str() << "\n" << str <<"\n"<< order_038 <<"\n";
-                    
-                //     file1.close();  
-                // }
-
-                for (int i_sample = 0; i_sample < 230; i_sample++){
-
-                    infile_Torque_st[j_lib][i_lib] >> Torque_Profile_st[j_lib][i_lib][i_sample];//read a value(jump to another value when meet a space or tab)
-                }
-                infile_Torque_st[j_lib][i_lib].close();//close the file after read
-            }
-
-            //debugging tool//////////////////////////////
-            // std::ofstream file("Torque_Profile_st.txt");
-            //             for (int k = 0; k < 230; ++k) {
-            //                 file << Torque_Profile_st[11][14][k];
-            //                 if (k != 229) file << " ";  // Separate values with spaces
-            //             }
-            // file.close(); 
-
-            //////////// For Front right leg ////////////////
-
-            // std::stringstream filename_torque_FRthigh;
-            // filename_torque_FRthigh << "/home/dlar/Gait_Library/Torquelib0723/Torque_Thigh_v"<< str << ".txt";
-	        // infile_Torque_st_FRThigh[i_lib].open(filename_torque_FRthigh.str());//open the file
-
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    //     infile_Torque_st_FRThigh[i_lib] >> Torque_Profile_st_FRThigh[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_FRThigh[i_lib].close();//close the file after read
-
-            // std::stringstream filename_torque_FRcalf;
-            // filename_torque_FRcalf << "/home/dlar/Gait_Library/Torquelib0723/Torque_Calf_v"<< str << ".txt";
-	        // infile_Torque_st_FRCalf[i_lib].open(filename_torque_FRcalf.str());//open the file
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    // infile_Torque_st_FRCalf[i_lib] >> Torque_Profile_st_FRCalf[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_FRCalf[i_lib].close();//close the file after read   
-
-            // //////////// For Front left leg ////////////////
-
-            // std::stringstream filename_torque_FLthigh;
-            // filename_torque_FLthigh << "/home/dlar/Gait_Library/Torquelib0723/Torque_Thigh_v"<< str << ".txt";
-	        // infile_Torque_st_FLThigh[i_lib].open(filename_torque_FLthigh.str());//open the file
-
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    //     infile_Torque_st_FLThigh[i_lib] >> Torque_Profile_st_FLThigh[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_FLThigh[i_lib].close();//close the file after read
-
-            // std::stringstream filename_torque_FLcalf;
-            // filename_torque_FLcalf << "/home/dlar/Gait_Library/Torquelib0723/Torque_Calf_v"<< str << ".txt";
-	        // infile_Torque_st_FLCalf[i_lib].open(filename_torque_FLcalf.str());//open the file
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    // infile_Torque_st_FLCalf[i_lib] >> Torque_Profile_st_FLCalf[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_FLCalf[i_lib].close();//close the file after read   
-
-            // //////////// For Rear Right leg ////////////////
-
-            // std::stringstream filename_torque_RRthigh;
-            // filename_torque_RRthigh << "/home/dlar/Gait_Library/Torquelib0723/Torque_Thigh_v"<< str << ".txt";
-	        // infile_Torque_st_RRThigh[i_lib].open(filename_torque_RRthigh.str());//open the file
-
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    //     infile_Torque_st_RRThigh[i_lib] >> Torque_Profile_st_RRThigh[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_RRThigh[i_lib].close();//close the file after read
-
-            // std::stringstream filename_torque_RRcalf;
-            // filename_torque_RRcalf << "/home/dlar/Gait_Library/Torquelib0723/Torque_Calf_v"<< str << ".txt";
-	        // infile_Torque_st_RRCalf[i_lib].open(filename_torque_RRcalf.str());//open the file
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    // infile_Torque_st_RRCalf[i_lib] >> Torque_Profile_st_RRCalf[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_RRCalf[i_lib].close();//close the file after read
-
-            // //////////// For Rear left leg ////////////////
-
-            // std::stringstream filename_torque_RLthigh;
-            // filename_torque_RLthigh << "/home/dlar/Gait_Library/Torquelib0723/Torque_Thigh_v"<< str << ".txt";
-	        // infile_Torque_st_RLThigh[i_lib].open(filename_torque_RLthigh.str());//open the file
-
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    //     infile_Torque_st_RLThigh[i_lib] >> Torque_Profile_st_RLThigh[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_RLThigh[i_lib].close();//close the file after read
-
-            // std::stringstream filename_torque_RLcalf;
-            // filename_torque_RLcalf << "/home/dlar/Gait_Library/Torquelib0723/Torque_Calf_v"<< str << ".txt";
-	        // infile_Torque_st_RLCalf[i_lib].open(filename_torque_RLcalf.str());//open the file
-	        // for (int i_bez = 0; i_bez < 230; i_bez++){
-
-		    // infile_Torque_st_RLCalf[i_lib] >> Torque_Profile_st_RLCalf[i_lib][i_bez];//read a value(jump to another value when meet a space or tab)
-            // }
-	        // infile_Torque_st_RLCalf[i_lib].close();//close the file after read
-            //////////////////////////////////////////////////////////////////////////////////////
-    }
-
-
-}
 
 // This is the function
 void Torqueprofile_interpolation(float (&Torque_Profile_st)[12][21][230],float (&ILC_torque)[12], std::vector<float> &x_velocity, float fly_sel_velocity, float (&desired_velocity_trunk)[3], int int_pre_number, long motiontime)
